72fd0bb81657e6e09e7ba2e67118479c9405ebab
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-03 16:44:05 -0300

    Update main.js

--- teste-integracao/main.js
+++ teste-integracao/main.js
@@ -2,8 +2,22 @@
 //valor = 100
 //desconto = 10
 // valor total = 90
+
 function calcularDesconto(valor) {
   return valor * 0.1;
 }
 
-console.log(calcularDesconto(1000));
+//teste de integração
+function calcularTotalCompra(valor) {
+  const desconto = calcularDesconto(valor);
+  return valor - desconto;
+}
+
+console.assert(
+  calcularTotalCompra(100) == 90,
+  "O valor dessa compra deveria ser 90",
+);
+console.assert(
+  calcularTotalCompra(100) == 10,
+  "O valor dessa compra deveria ser 90",
+);
