0951ee1d299f051f4987b393e6eaded4b918e851
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-03-04 14:19:06 -0300

    Update main.js

--- teste-unidade/main.js
+++ teste-unidade/main.js
@@ -3,6 +3,6 @@ function calcularJuros(valor, taxa) {
 }
 
 //Teste de unidade feito com sucesso
-console.assert(calcularJuros(100, 0.1) === 10, "jdoiajoijso");
+console.assert(calcularJuros(100, 0.1) === 10, "erro juros invalido");
 
-console.assert(calcularJuros(100, 0.1) === 100, "Erro, duhasudh");
+console.assert(calcularJuros(100, 0.1) === 100, "Erros");
